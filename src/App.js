0d42f30bc9c2817d1db35c18c5119d44f5d370d7
kishore007k <kishorekumarstar07@gmail.com>
2020-08-20 16:53:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NoMatch from './components/NoMatch';
+import Layout from './components/Layout';
+import NavigationBar from './components/NavigationBar';
+import Jumbotron from './components/Jumbotron';
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavigationBar />
+      <Jumbotron />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path ='/' component={Home} />
+            <Route exact path ='/about' component={About} />
+            <Route exact path ='/contact' component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </React.Fragment>
+  );
+}
+
+export default App;
